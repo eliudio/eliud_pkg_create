29b1c2052a9a8639de0458c0fda74b6cb1d49407
Eliud <58745656+eliudio@users.noreply.github.com>
2021-11-07 00:17:37 +0000

    Rework access bloc

--- lib/creator_package.dart
+++ lib/creator_package.dart
@@ -14,9 +14,6 @@ abstract class CreatorPackage extends Package {
   CreatorPackage() : super('eliud_pkg_fundamentals');
 
   @override
-  BlocProvider? createMainBloc(NavigatorBloc navigatorBloc, AccessBloc accessBloc) => null;
-
-  @override
   Future<bool?> isConditionOk(String packageCondition, AppModel app, MemberModel? member, bool isOwner, bool? isBlocked, PrivilegeLevel? privilegeLevel) async => null;
 
   @override
