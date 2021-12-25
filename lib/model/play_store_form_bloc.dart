79e2ddd62662c2d9f8749bc12cb4d7c6ecd37209
Eliud <58745656+eliudio@users.noreply.github.com>
2021-12-25 13:19:37 +0000

    Bugfix

--- lib/model/play_store_form_bloc.dart
+++ lib/model/play_store_form_bloc.dart
@@ -93,17 +93,16 @@ class PlayStoreFormBloc extends Bloc<PlayStoreFormEvent, PlayStoreFormState> {
         return;
       }
       if (event is ChangedPlayStoreItemBackground) {
-        if (event.value != null) {
+        if (event.value != null)
           newValue = currentState.value!.copyWith(itemBackground: await backgroundRepository(appId: appId)!.get(event.value));
-        } else {
-          newValue = PlayStoreModel(
+        else
+          newValue = new PlayStoreModel(
                                  documentID: currentState.value!.documentID,
                                  appId: currentState.value!.appId,
                                  description: currentState.value!.description,
                                  itemBackground: null,
                                  conditions: currentState.value!.conditions,
           );
-        }
         yield SubmittablePlayStoreForm(value: newValue);
 
         return;
