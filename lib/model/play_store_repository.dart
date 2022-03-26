d90714170a64671bac53eb7f152e1a62992c1115
Eliud <58745656+eliudio@users.noreply.github.com>
2022-03-26 10:17:18 +0000

    Introduce template for editors!

--- lib/model/play_store_repository.dart
+++ lib/model/play_store_repository.dart
@@ -32,11 +32,12 @@ import 'package:eliud_pkg_create/model/entity_export.dart';
 import 'dart:async';
 import 'package:eliud_core/tools/query/query_tools.dart';
 import 'package:eliud_core/tools/common_tools.dart';
+import 'package:eliud_core/tools/repository_base.dart';
 
 typedef PlayStoreModelTrigger(List<PlayStoreModel?> list);
 typedef PlayStoreChanged(PlayStoreModel? value);
 
-abstract class PlayStoreRepository {
+abstract class PlayStoreRepository extends RepositoryBase<PlayStoreModel> {
   Future<PlayStoreModel> add(PlayStoreModel value);
   Future<void> delete(PlayStoreModel value);
   Future<PlayStoreModel?> get(String? id, { Function(Exception)? onError });
