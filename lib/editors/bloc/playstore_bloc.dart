d4599f02994ffc89c6b8ac5eae08047df21c2ab5
Eliud <58745656+eliudio@users.noreply.github.com>
2022-03-29 18:40:14 +0100

    refactor background widget to use within other widgets, other than mona style

---
+++ lib/editors/bloc/playstore_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:eliud_core/model/storage_conditions_model.dart';
+import 'package:eliud_core/tools/component/component_spec.dart';
+import 'package:eliud_core/tools/random.dart';
+import 'package:eliud_core/core/editor/editor_base_bloc/editor_base_bloc.dart';
+import 'package:eliud_pkg_create/model/abstract_repository_singleton.dart';
+import 'package:eliud_pkg_create/model/play_store_model.dart';
+
+class PlayStoreBloc extends EditorBaseBloc<PlayStoreModel> {
+  PlayStoreBloc(String appId, EditorFeedback feedback)
+      : super(appId, playStoreRepository(appId: appId)!, feedback);
+
+  @override
+  PlayStoreModel newInstance(StorageConditionsModel conditions) {
+    return PlayStoreModel(
+        appId: appId,
+        documentID: newRandomKey(),
+        conditions: conditions,
+        description: 'new play store');
+  }
+
+  @override
+  PlayStoreModel setDefaultConditions(
+      PlayStoreModel t, StorageConditionsModel conditions) {
+    return t.copyWith(
+        conditions: t.conditions ??
+            StorageConditionsModel(
+                privilegeLevelRequired:
+                PrivilegeLevelRequiredSimple.NoPrivilegeRequiredSimple));
+  }
+}
--- lib/editors/play_store_component_editor.dart
+++ lib/editors/play_store_component_editor.dart
@@ -1,23 +1,182 @@
-import 'package:eliud_core/core/blocs/access/access_bloc.dart';
+import 'package:eliud_core/core/blocs/access/state/access_determined.dart';
+import 'package:eliud_core/core/blocs/access/state/access_state.dart';
 import 'package:eliud_core/model/app_model.dart';
+import 'package:eliud_core/model/storage_conditions_model.dart';
+import 'package:eliud_core/style/frontend/has_container.dart';
 import 'package:eliud_core/style/frontend/has_dialog.dart';
+import 'package:eliud_core/style/frontend/has_dialog_field.dart';
+import 'package:eliud_core/style/frontend/has_list_tile.dart';
+import 'package:eliud_core/style/frontend/has_progress_indicator.dart';
+import 'package:eliud_core/style/frontend/has_text.dart';
 import 'package:eliud_core/tools/component/component_spec.dart';
+import 'package:eliud_core/tools/random.dart';
+import 'package:eliud_core/tools/rgb_formfield.dart';
+import 'package:eliud_core/tools/widgets/condition_simple_widget.dart';
+import 'package:eliud_core/tools/widgets/header_widget.dart';
+import 'package:eliud_pkg_create/model/abstract_repository_singleton.dart';
+import 'package:eliud_pkg_create/model/play_store_model.dart';
+import 'package:eliud_pkg_medium/editors/widgets/background_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:eliud_core/core/blocs/access/access_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:eliud_core/core/editor/editor_base_bloc/editor_base_bloc.dart';
+import 'package:eliud_core/core/editor/editor_base_bloc/editor_base_event.dart';
+import 'package:eliud_core/core/editor/editor_base_bloc/editor_base_state.dart';
+
+import 'bloc/playStore_bloc.dart';
+import 'bloc/playstore_bloc.dart';
 
 class PlayStoreComponentEditorConstructor extends ComponentEditorConstructor {
   @override
-  void updateComponent(AppModel app, BuildContext context, model, EditorFeedback feedback) {
-    openErrorDialog(app, context, app.documentID! + '/_error', title: 'Problem', errorMessage: 'No editor for this component available yet');
+  void updateComponent(
+      AppModel app, BuildContext context, model, EditorFeedback feedback) {
+    _openIt(app, context, false, model.copyWith(), feedback);
   }
 
   @override
-  void createNewComponent(AppModel app, BuildContext context, EditorFeedback feedback) {
-    openErrorDialog(app, context, app.documentID! + '/_error', title: 'Problem', errorMessage: 'No editor for this component available yet');
+  void createNewComponent(
+      AppModel app, BuildContext context, EditorFeedback feedback) {
+    _openIt(
+        app,
+        context,
+        true,
+        PlayStoreModel(
+          appId: app.documentID,
+          documentID: newRandomKey(),
+          conditions: StorageConditionsModel(
+              privilegeLevelRequired:
+              PrivilegeLevelRequiredSimple.NoPrivilegeRequiredSimple),
+        ),
+        feedback);
   }
 
   @override
-  void updateComponentWithID(AppModel app, BuildContext context, String id, EditorFeedback feedback) {
-    openErrorDialog(app, context, app.documentID! + '/_error', title: 'Problem', errorMessage: 'No editor for this component available yet');
+  void updateComponentWithID(AppModel app, BuildContext context, String id,
+      EditorFeedback feedback) async {
+    var playStore = await playStoreRepository(appId: app.documentID!)!.get(id);
+    if (playStore != null) {
+      _openIt(app, context, false, playStore, feedback);
+    } else {
+      openErrorDialog(app, context, app.documentID! + '/_error',
+          title: 'Error',
+          errorMessage: 'Cannot find notification dashboard with id $id');
+    }
+  }
+
+  void _openIt(AppModel app, BuildContext context, bool create,
+      PlayStoreModel model, EditorFeedback feedback) {
+    openComplexDialog(
+      app,
+      context,
+      app.documentID! + '/notificationdashboard',
+      title: create
+          ? 'Create Notification Dashboard'
+          : 'Update Notification Dashboard',
+      includeHeading: false,
+      widthFraction: .9,
+      child: BlocProvider<PlayStoreBloc>(
+          create: (context) => PlayStoreBloc(
+            app.documentID!,
+            /*create,
+            */
+            feedback,
+          )..add(EditorBaseInitialise<PlayStoreModel>(model)),
+          child: PlayStoreComponentEditor(
+            app: app,
+          )),
+    );
+  }
+}
+
+class PlayStoreComponentEditor extends StatefulWidget {
+  final AppModel app;
+
+  const PlayStoreComponentEditor({
+    Key? key,
+    required this.app,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _PlayStoreComponentEditorState();
+}
+
+class _PlayStoreComponentEditorState extends State<PlayStoreComponentEditor> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AccessBloc, AccessState>(
+        builder: (aContext, accessState) {
+          if (accessState is AccessDetermined) {
+            return BlocBuilder<PlayStoreBloc, EditorBaseState<PlayStoreModel>>(
+                builder: (ppContext, playStoreState) {
+                  if (playStoreState is EditorBaseInitialised<PlayStoreModel>) {
+                    return ListView(
+                        shrinkWrap: true,
+                        physics: ScrollPhysics(),
+                        children: [
+                          HeaderWidget(
+                            app: widget.app,
+                            title: 'PlayStore',
+                            okAction: () async {
+                              await BlocProvider.of<PlayStoreBloc>(context).save(
+                                  EditorBaseApplyChanges<PlayStoreModel>(
+                                      model: playStoreState.model));
+                              return true;
+                            },
+                            cancelAction: () async {
+                              return true;
+                            },
+                          ),
+                          topicContainer(widget.app, context,
+                              title: 'General',
+                              collapsible: true,
+                              collapsed: true,
+                              children: [
+                                getListTile(context, widget.app,
+                                    leading: Icon(Icons.vpn_key),
+                                    title: text(widget.app, context,
+                                        playStoreState.model.documentID!)),
+                                getListTile(context, widget.app,
+                                    leading: Icon(Icons.description),
+                                    title: dialogField(
+                                      widget.app,
+                                      context,
+                                      initialValue: playStoreState.model.description,
+                                      valueChanged: (value) {
+                                        playStoreState.model.description = value;
+                                      },
+                                      maxLines: 1,
+                                      decoration: const InputDecoration(
+                                        hintText: 'Description',
+                                        labelText: 'Description',
+                                      ),
+                                    )),
+                              ]),
+                          BackgroundWidget(
+                              app: widget.app,
+                              memberId: widget.memberId,
+                              value: playStoreState.model.itemBackground!,
+                              label: 'Background'),
+                          topicContainer(widget.app, context,
+                              title: 'Condition',
+                              collapsible: true,
+                              collapsed: true,
+                              children: [
+                                getListTile(context, widget.app,
+                                    leading: Icon(Icons.security),
+                                    title: ConditionsSimpleWidget(
+                                      app: widget.app,
+                                      value: playStoreState.model.conditions!,
+                                    )),
+                              ]),
+                        ]);
+                  } else {
+                    return progressIndicator(widget.app, context);
+                  }
+                });
+          } else {
+            return progressIndicator(widget.app, context);
+          }
+        });
   }
 }
