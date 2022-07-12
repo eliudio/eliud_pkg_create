e856a3428869284dcde923c3615ff200d6679fc6
Eliud <58745656+eliudio@users.noreply.github.com>
2022-07-12 15:51:33 +0100

    Bugfixes

--- lib/jsontomodeltojson/jsonconst.dart
+++ lib/jsontomodeltojson/jsonconst.dart
@@ -15,7 +15,7 @@ class JsonConsts {
   static String app = 'app';
   static String appBar = 'appBar';
   static String homeMenu = 'homeMenu';
-  static String leftDrawer = 'leftDrawer';
+  static String leftDrawerX = 'leftDrawer';
   static String rightDrawer = 'rightDrawer';
   static String dialogs = 'dialogs';
   static String pages = 'pages';
